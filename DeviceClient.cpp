1dc1082feae0e8e5bae3383a8425cc6bc51112da
Albert Nadal <a.nadal@iskra.cat>
2018-01-08 12:35:22 +0100

    Implemented connect method and onConnect and onConnectError callbacks

--- DeviceClient.cpp
+++ DeviceClient.cpp
@@ -21,14 +21,13 @@ struct device_token_t {
         unsigned int device_id;
 };
 
-struct connect_device_data_t {
-        DeviceClient::device_token_t* device_token_data;
-        string socket_io_namespace;
-};
-
+bool isConnected;
 DeviceClient(const string& apiKey, const string& deviceKey, const string& hostname);
 void connect(const string& hostname);
-rxcpp::observable<connect_device_data_t*> connectToServer(const string& hostname, unsigned int apiPort, unsigned int webSocketPort, const string& deviceToken, const string& apiKey, const string& deviceKey);
+void reconnect();
+void onConnect(void (*callback)());
+void onConnectError(void (*callback)(const std::exception& ex));
+rxcpp::observable<int> connectToServer(const string& hostname, unsigned int apiPort, unsigned int webSocketPort, const string& deviceToken, const string& apiKey, const string& deviceKey);
 
 private:
 
@@ -38,18 +37,29 @@ string host;
 unsigned int apiPort;
 unsigned int webSocketPort;
 string dbFilename;
+string deviceToken;
+string socketIOnamespace;
+unsigned int deviceId;
 static const vector<string> explode(const string& s, const char& c);
+void (*onConnectCallback)();
+void (*onConnectErrorCallback)(const std::exception& ex);
 device_token_t* getDeviceTokenWithAPIKeyAndDeviceKey(const string& hostname, unsigned int apiPort, const string& apiKey, const string& deviceKey);
 
 };
 
 DeviceClient::DeviceClient(const string& apiKey, const string& deviceKey, const string& hostname) {
+        this->isConnected = false;
         this->apiKey = apiKey;
         this->deviceKey = deviceKey;
         this->host = hostname;
         this->apiPort = 3001;
         this->webSocketPort = 3000;
+        this->deviceId = 0;
         this->dbFilename = "albia.sqlite";
+        this->deviceToken = "";
+        this->socketIOnamespace = "";
+        this->onConnectCallback = NULL;
+        this->onConnectErrorCallback = NULL;
 }
 
 const vector<string> DeviceClient::explode(const string& s, const char& c) {
@@ -63,6 +73,14 @@ const vector<string> DeviceClient::explode(const string& s, const char& c) {
         if(buff != "") v.push_back(buff);
 
         return v;
+}
+
+void DeviceClient::onConnect(void (*callback)()) {
+   this->onConnectCallback = callback;
+}
+
+void DeviceClient::onConnectError(void (*callback)(const std::exception& ex)) {
+   this->onConnectErrorCallback = callback;
 }
 
 DeviceClient::device_token_t* DeviceClient::getDeviceTokenWithAPIKeyAndDeviceKey(const string& hostname, unsigned int apiPort, const string& apiKey, const string& deviceKey) {
@@ -114,35 +132,71 @@ DeviceClient::device_token_t* DeviceClient::getDeviceTokenWithAPIKeyAndDeviceKey
         return NULL;
 }
 
-void connect(const string& hostname) {
+void DeviceClient::connect(const string& hostname = "") {
+
+  if(this->isConnected) {
+    return;
+  }
+
+  if(hostname != "") {
+    this->host = hostname;
+  }
+
+  rxcpp::observable<int> observable = this->connectToServer(this->host, this->apiPort, this->webSocketPort, this->deviceToken, this->apiKey, this->deviceKey);
+
+  observable.subscribe(
+          [](int v) {
+  },
+          [this](std::exception_ptr ep){
+          this->isConnected = false;
+          try {std::rethrow_exception(ep);}
+          catch (const std::exception& ex) {
+                  if(this->onConnectErrorCallback != NULL) {
+                    (*this->onConnectErrorCallback)(ex);
+                  }
+          }
+  },
+          [this](){
+            cout << "DEVICE ID: " << this->deviceId << " DEVICE TOKEN: " << this->deviceToken << endl << "NAMESPACE: " << this->socketIOnamespace << endl;
+            this->isConnected = true;
+/*
+            $this->startWriteQueueThread();
+            $this->startHeartBeatTimer();
+*/
+            if(this->onConnectCallback != NULL) {
+              (*this->onConnectCallback)();
+            }
+/*
+            // Launch the event loop of the socketIO
+            $this->runLoop();
+*/
+  });
 
 }
 
-rxcpp::observable<DeviceClient::connect_device_data_t*> DeviceClient::connectToServer(const string& hostname, unsigned int apiPort, unsigned int webSocketPort, const string& deviceToken, const string& apiKey, const string& deviceKey) {
+void DeviceClient::reconnect() {
+   this->connect(this->host);
+}
 
-        static rxcpp::observable<DeviceClient::connect_device_data_t*> observable = rxcpp::observable<>::create<DeviceClient::connect_device_data_t*>(
-                [this, &deviceToken, &hostname, apiPort, &apiKey, &deviceKey](rxcpp::subscriber<DeviceClient::connect_device_data_t*> observer){
+rxcpp::observable<int> DeviceClient::connectToServer(const string& hostname, unsigned int apiPort, unsigned int webSocketPort, const string& deviceToken, const string& apiKey, const string& deviceKey) {
+
+        static rxcpp::observable<int> observable = rxcpp::observable<>::create<int>(
+                [this, &deviceToken, &hostname, apiPort, &apiKey, &deviceKey](rxcpp::subscriber<int> observer){
                 try {
-
                         cout << "Connecting to server..." << endl;
-
-                        DeviceClient::connect_device_data_t* connectDeviceData = new DeviceClient::connect_device_data_t;
-                        connectDeviceData->device_token_data = NULL;
-
                         string localDeviceToken("");
 
                         if(deviceToken.empty()) {
                                 device_token_t* deviceTokenData = this->getDeviceTokenWithAPIKeyAndDeviceKey(hostname, 3001, apiKey, deviceKey);
-
                                 if(deviceTokenData != NULL) {
                                         localDeviceToken = deviceTokenData->token;
-                                        connectDeviceData->device_token_data = deviceTokenData;
+                                        this->deviceId = deviceTokenData->device_id;
                                 }
-
                         } else {
                                 localDeviceToken = deviceToken;
                         }
 
+                        this->deviceToken = localDeviceToken;
                         curlpp::Cleanup cleaner;
                         curlpp::Easy request;
                         string scheme("http://");
@@ -169,12 +223,9 @@ rxcpp::observable<DeviceClient::connect_device_data_t*> DeviceClient::connectToS
                         Document d;
                         d.Parse(jsonResponse.c_str());
                         Value& s = d["namespace"];
-                        string socketIOnamespace = s.GetString();
-                        connectDeviceData->socket_io_namespace = socketIOnamespace;
+                        this->socketIOnamespace = s.GetString();
 
-                        observer.on_next(connectDeviceData);
                         observer.on_completed();
-
                 } catch( curlpp::RuntimeError &e ) {
                         cout << e.what() << endl;
                         observer.on_error(std::current_exception());
@@ -193,32 +244,18 @@ int main(int, char **)
         string hostname("maduixa.lafruitera.com");
         string apiKey("app1234");
         string deviceKey("key1234");
-        string deviceToken("");
-        string socketIOnamespace("");
-        unsigned int deviceId = 0;
 
         DeviceClient *client = new DeviceClient(apiKey, deviceKey, hostname);
 
-        rxcpp::observable<DeviceClient::connect_device_data_t*> observable = client->connectToServer(hostname, 3001, 3000, deviceToken, apiKey, deviceKey);
-
-        observable.subscribe(
-                [&deviceToken, &socketIOnamespace, &deviceId](DeviceClient::connect_device_data_t* connectDeviceData) {
-                deviceToken = connectDeviceData->device_token_data->token;
-                deviceId = connectDeviceData->device_token_data->device_id;
-                delete connectDeviceData->device_token_data;
-                socketIOnamespace = connectDeviceData->socket_io_namespace;
-                delete connectDeviceData;
-                cout << "DEVICE TOKEN: " << deviceToken << endl << "NAMESPACE: " << socketIOnamespace << endl;
-        },
-                [](std::exception_ptr ep){
-                try {std::rethrow_exception(ep);}
-                catch (const std::exception& ex) {
-                        printf("OnError: %s\n", ex.what());
-                }
-        },
-                [](){
-
+        client->onConnect([]() {
+          cout << "Connected!" << endl;
         });
+
+        client->onConnectError([](const std::exception& ex) {
+          cout << "ERROR when connecting: " << ex.what() << endl;
+        });
+
+        client->connect();
 
         return 0;
 }
